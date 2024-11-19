b3f60a985c59c3215e555ab0140cfa26a0345c30
Jonathan Leimbach <jonathan.leimbach2408@gmail.com>
2024-11-19 10:34:07 +0100

    solved fibonacci-seq. && mergesort

--- recursion/fibonacci.js
+++ recursion/fibonacci.js
@@ -1,11 +1,26 @@
-const fib = (n) => {
+// const fib = (n) => {
+//   if (n <= 0) return [];
+//   if (n === 1) return [0];
+//   if (n === 2) return [0, 1];
+
+//   let arr = [0, 1];
+//   while (arr.length < n) {
+//     arr.push(arr[arr.length - 1] + arr[arr.length - 2]);
+//   }
+//   return arr;
+// };
+
+// const testFib = fib(19);
+// console.log(testFib);
+
+const fibRec = (n) => {
   // base case
   if (n === 0) return 0;
   if (n === 1) return 1;
 
   // recursive case
-  return fib(n - 1) + fib(n - 2);
+  return fibRec(n - 1) + fibRec(n - 2);
 };
 
-const test = fib(8);
-console.log(test);
+const testFibRec = fibRec(8);
+console.log(testFibRec);
--- recursion/mergesort.js
+++ recursion/mergesort.js
@@ -1,4 +1,33 @@
-const mergesort = (arr) => {
+const sort = (arr) => {
   // Base Case
+  // arr.length = 1 -> already sorted -> return
+  if (arr.length <= 1) {
+    return arr;
+  }
+
   // Recursive Case
+  const middle = Math.floor(arr.length / 2);
+  const left = sort(arr.slice(0, middle));
+  const right = sort(arr.slice(middle));
+
+  return merge(left, right);
 };
+
+const merge = (left, right) => {
+  let result = [];
+  let i = 0;
+  let j = 0;
+
+  while (i < left.length && j < right.length) {
+    if (left[i] <= right[j]) {
+      result.push(left[i++]);
+    } else {
+      result.push(right[j++]);
+    }
+  }
+
+  return result.concat(left.slice(i), right.slice(j));
+};
+
+const test = sort([10, 5, 7, 2, 4, 1, 8, 9, 3, 6]);
+console.log(test);
